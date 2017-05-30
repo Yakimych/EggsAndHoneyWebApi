47d4ea0c2496cb8936b57afe8e4c925aaae95f7b
Kyrylo Yakymenko <yakimych@gmail.com>
2017-05-30 23:24:18 +0200

    Fixes #2 - Added Entity Framework migrations (and created the database).

---
+++ src/EggsAndHoney.Domain/Migrations/20170530204058_InitialCreate.Designer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using EggsAndHoney.Domain.Models;
+
+namespace EggsAndHoney.Domain.Migrations
+{
+    [DbContext(typeof(OrderContext))]
+    [Migration("20170530204058_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("EggsAndHoney.Domain.Models.Order", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DatePlaced");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    b.Property<int>("OrderTypeId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("OrderTypeId");
+
+                    b.ToTable("Orders");
+                });
+
+            modelBuilder.Entity("EggsAndHoney.Domain.Models.OrderType", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("OrderTypes");
+                });
+
+            modelBuilder.Entity("EggsAndHoney.Domain.Models.ResolvedOrder", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("DatePlaced");
+
+                    b.Property<DateTime>("DateResolved");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    b.Property<int>("OrderTypeId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("OrderTypeId");
+
+                    b.ToTable("ResolvedOrders");
+                });
+
+            modelBuilder.Entity("EggsAndHoney.Domain.Models.Order", b =>
+                {
+                    b.HasOne("EggsAndHoney.Domain.Models.OrderType", "OrderType")
+                        .WithMany()
+                        .HasForeignKey("OrderTypeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("EggsAndHoney.Domain.Models.ResolvedOrder", b =>
+                {
+                    b.HasOne("EggsAndHoney.Domain.Models.OrderType", "OrderType")
+                        .WithMany()
+                        .HasForeignKey("OrderTypeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
