35659b6d6b69c431b4d3b3c7900f812e942b3b65
Kyrylo Yakymenko <yakimych@gmail.com>
2017-12-28 01:40:33 +0100

    Add test endpoints and swagger docs, update nuget packages in tests

---
+++ src/EggsAndHoney.WebApi.FSharp/Controllers/OrdersController.fs
@@ -0,0 +1,20 @@
+﻿namespace EggsAndHoney.WebApi.FSharp.Controllers
+
+open System
+open Microsoft.AspNetCore.Mvc
+open EggsAndHoney.WebApi.FSharp.ViewModels
+
+[<Route("api/v1/[controller]")>]
+type OrdersController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Get() =
+        let fakeOrdersViewModel = { Id = 1; Name = "Rita"; Order = "Honey"; DatePlaced = DateTime.Now }
+        let fakeOrdersViewModel' = { Id = 2; Name = "YaK"; Order = "Eggs"; DatePlaced = DateTime.Now }
+        let ordersViewModels = [fakeOrdersViewModel; fakeOrdersViewModel']
+        this.Ok(ordersViewModels)
+
+    [<HttpGet("count")>]
+    member this.GetCount() =
+        this.Ok(4)
--- src/EggsAndHoney.WebApi.FSharp/Startup.fs
+++ src/EggsAndHoney.WebApi.FSharp/Startup.fs
@@ -8,7 +8,7 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
-
+open Swashbuckle.AspNetCore.Swagger
 
 type Startup private () =
     new (configuration: IConfiguration) as this =
@@ -19,9 +19,18 @@ type Startup private () =
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
         services.AddMvc() |> ignore
+        
+        services.AddSwaggerGen(fun c -> 
+            c.SwaggerDoc("v1", new Info (Title = "Eggs&Honey Web API", Version = "v1"))
+        ) |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
         app.UseMvc() |> ignore
+        
+        app.UseSwagger();
+        app.UseSwaggerUI(fun c ->
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eggs&Honey Web API V1")
+        ) |> ignore
 
     member val Configuration : IConfiguration = null with get, set
